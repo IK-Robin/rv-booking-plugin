bd6eb49f4c8c3438ccddaa45d0ea766ae7edfcee
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-12-19 12:27:57 +0600

    add new file

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * rv_booking_system plugin 
+ *
+ * @package rv_booking_system
+ * 
+ * @link 
+ * @see
+ */
+
+
+ /**
+  * @link 
+  * @see  /rv-booking-menu.php
+  */
+
+require RV_BOOKING_STYTEM_PATH . '/includes/rv-booking-menu.php';
---
+++ includes/importent_shortcordes.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * rv_booking_system plugin 
+ *
+ * @package rv_booking_system
+ * 
+ * @link 
+ * @see
+ */
+
+ function rv_booking_system_all_shortcode (){
+ 
+
+    echo 'shortcode one';
+    echo RV_BOOKING_STYTEM_PATH;
+ }
